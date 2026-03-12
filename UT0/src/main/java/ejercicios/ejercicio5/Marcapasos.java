beb66013abeb78f7ab6a694d4429aaf6f9de31d3
Juan Baldenegro <132092063+juanBalde2002@users.noreply.github.com>
2026-03-12 19:35:33 -0300

    falta crear el equals

---
+++ UT0/src/main/java/ejercicios/ejercicio5/Marcapasos.java
@@ -0,0 +1,54 @@
+package ejercicios.ejercicio5;
+
+public class Marcapasos {
+    private int idDispositivo;
+    private String codigoFabricante;
+    private int latidosPorMinuto;
+    private int nivelBateria;
+
+    public static int contadorInstancias = 0; //controla can de instancias creadas
+
+
+    //CONSTRUCTOR
+    public Marcapasos(int id,String cod,int lats){
+        this.idDispositivo = id  ;
+        this.codigoFabricante=cod + "cte"; //se agrega el cte de fabrica a cada instancia
+        this.latidosPorMinuto=lats;
+        this.nivelBateria=100;
+        contadorInstancias++; //cada vez que se crea una isntancia se sube el valor del contador
+    }
+
+    //Tanto el id como el codigo son atributos que una vez que son definidos no tiene sentido que sean reemplzables
+    //por lo contrario los latidos pueden verse modificados dependiendo de la situacion del paciente
+    //por ultimo, el nivel de bateria disminuye gradualemente y dependiendo como es implementado si se puede acceder o no al setter.
+
+    //GETTERS
+    public int GetId(){
+        return idDispositivo;
+    }
+    public String GetCodigoFab(){
+        return codigoFabricante;
+    }
+    public int GetLatidos(){
+        return latidosPorMinuto;
+    }
+    public int GetBateria(){
+        return nivelBateria;
+    }
+
+    //SETTERS
+    public void SetId(int unId){
+        this.idDispositivo = unId;
+    }
+    public void SetCodigo(String unCodigo){
+        this.codigoFabricante = unCodigo;
+    }
+
+    //METODOS
+    public void toString(Marcapasos unMarca){
+        System.out.println("ID: " + unMarca.idDispositivo +"\n" +
+                        "Codigo: "+ unMarca.codigoFabricante + "\n" +
+        "Latidos: " + unMarca.latidosPorMinuto + "\n"
+        + "nivel de bateria: "+ unMarca.nivelBateria + "\n");
+    }
+}
