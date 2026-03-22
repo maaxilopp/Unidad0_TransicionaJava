72cc69db9456ba6d957a2e0a755dd02ba6628733
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2026-03-22 20:59:40 -0300

    ejercicio 12

---
+++ UT0/src/main/java/ejercicios/ejercicio12/StringNull_mal.java
@@ -0,0 +1,9 @@
+package ejercicios.ejercicio12;
+
+public class StringNull_mal{
+
+    public static void main(String[] args) {
+        String nombre = null;
+        System.out.println(nombre.length());
+    }
+}
---
+++ UT0/src/main/java/ejercicios/ejercicio12/SumaAcumulada_corregido.java
@@ -0,0 +1,13 @@
+package ejercicios.ejercicio12;
+
+public class SumaAcumulada_corregido {
+
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 1; i <= 5; i++) {
+            suma += i;
+
+        }
+        System.out.println(suma);
+    }
+}
