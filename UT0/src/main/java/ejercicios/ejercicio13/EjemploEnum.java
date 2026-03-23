52184c28cb496bf8f722bb99ac8b3aaa7b6a6d10
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2026-03-23 00:38:14 -0300

    pequeñisimo arreglo

--- UT0/src/main/java/ejercicios/ejercicio13/EjemploEnum.java
+++ UT0/src/main/java/ejercicios/ejercicio13/EjemploEnum.java
@@ -5,13 +5,21 @@ public class EjemploEnum {
     enum TipoCaracter {VOCAL, CONSONANTE, DIGITO, OTRO}
 
     public static void main(String[] args) {
+
+        System.out.println("Valores del enum: ");
+        for (TipoCaracter t : TipoCaracter.values()) {
+            System.out.println(t);
+        }
+        System.out.println(" ");
         // Contadores, uno por cada categoría
         int contadorVocales = 0;
         int contadorConsonantes = 0;
         int contadorDigitos = 0;
         int contadorOtro = 0;
         String test = "AEIOÚ m.2007";
-        System.out.println("Texto Original: " + test);
+        System.out.println("Texto: " + test);
+        System.out.println(" ");
+
 
         // Convertimos todo a minúsculas para simplificar las comparaciones
         test = test.toLowerCase();
@@ -44,9 +52,9 @@ public class EjemploEnum {
         }
 
         // Mostramos los resultados finales
-        System.out.println("Vocales: " + contadorVocales);
-        System.out.println("Consonantes: " + contadorConsonantes);
-        System.out.println("Digitos: " + contadorDigitos);
-        System.out.println("Otros: " + contadorOtro);
+        System.out.println("Vocales del texto: " + contadorVocales);
+        System.out.println("Consonantes del texto: " + contadorConsonantes);
+        System.out.println("Digitos del texto: " + contadorDigitos);
+        System.out.println("Otros caracteres del texto: " + contadorOtro);
     }
 }
