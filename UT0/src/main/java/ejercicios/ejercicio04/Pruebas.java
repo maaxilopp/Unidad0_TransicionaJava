032c65157379b2bc8f3e0f4b868d74395b8ff3a8
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2026-03-22 22:56:04 -0300

    prueba excepción

--- UT0/src/main/java/ejercicios/ejercicio04/Pruebas.java
+++ UT0/src/main/java/ejercicios/ejercicio04/Pruebas.java
@@ -7,6 +7,7 @@ public class Pruebas {
 // factorial(5)  = 120
 // factorial(0)  = 1
 // factorial(1)  = 1
+// factorial(-1) = mensaje de excepción
 // isPrime(7)    = true, Suma de pares hasta 7: 12
 
     public static void main(String[] args) {
@@ -15,6 +16,11 @@ public class Pruebas {
         System.out.println("Factorial de 5: " + factorial(5));
         System.out.println("Factorial de 0: " + factorial(0));
         System.out.println("Factorial de 1: " + factorial(1));
+        try {
+            System.out.println("Factorial de -1: " + factorial(-1));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Factorial de -1: " + e.getMessage());
+        }
 
 
         long n = 7;
